9677ab9a27e4993dcc1368c5a66a35135cd6716d
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-30 12:53:12 +0200

    Add basic JS code

---
+++ script.js
@@ -0,0 +1,6 @@
+const $input = document.getElementById('from__input');
+const $button = document.getElementById('form__button');
+
+$button.addEventListener('click', (event) => {
+    event.preventDefault();
+});
