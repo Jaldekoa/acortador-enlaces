6191f897a55cf5ca210aa366d2aeded26d3588bf
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-31 23:01:01 +0200

    Add some fetch code

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const $input = document.getElementById('from__input');
+const $input = document.getElementById('form__input');
 const $button = document.getElementById('form__button');
 
-$button.addEventListener('click', (event) => {
+$button.addEventListener('click', async (event) => {
     event.preventDefault();
+    const WORKER_URL = 'https://acortador-enlaces.jaldekoa.workers.dev/';
+
+    const longURL = new URL($input.value).href;
+    if (!longURL || longURL === '') throw new Error(`${$input.value} no es una URL válida`);
+
+    $button.disabled = true;
+
+    try {
+        const OPTIONS = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ longURL }) }
+        const response = await fetch(WORKER_URL, OPTIONS);
+
+        if (!response.ok)
+            throw new Error(`HTTP error ${response.status}`);
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        $button.disabled = false;
+    }
+
 });
