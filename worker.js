4e4e1c58a4e845b91e7deb4a4ca97dad3fb2b327
Jon Aldekoa <jaldekoa@gmail.com>
2026-03-31 23:01:13 +0200

    Add basic worker code

--- worker.js
+++ worker.js
@@ -1,5 +1,32 @@
 export default {
-    async fetch(request, env, ctx) {
-        return new Response("Hello World!");
-    },
+    async fetch(request) {
+
+        const corsHeaders = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            "Content-Type": "application/json",
+        };
+
+        if (request.method === "OPTIONS") {
+            return new Response(null, { status: 204, headers: corsHeaders });
+        };
+
+        // 2. Tu lógica normal
+        if (request.method === 'POST' && request.url === 'https://acortador-enlaces.pages.dev') {
+            const body = await request.json();
+
+            return new Response(JSON.stringify({ ok: true }), {
+                headers: {
+                    ...corsHeaders,
+                    'Content-Type': 'application/json'
+                }
+            });
+        }
+
+        return new Response('Not found', {
+            status: 404,
+            headers: corsHeaders
+        });
+    }
 };
